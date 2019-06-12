62f7dc5fb84b18928c406f02b16f3e321ed5986a
Kunal Tiwari <kunalltiwari@gmail.com>
2019-06-12 11:58:54 -0400

    Added a couple pages to see how I need to modify app.py to let Heroku tell the website what to do. `interactive' elements in query.html point to nowhere, need to figure out how these should be linked up to python code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+#decorator from Flask accepts route (where in the website you are) and directs to the correct html. Must function name match html file?
 @app.route('/')
 def index():
   return render_template('index.html') #requires template file ot be in ./templates/
@@ -9,6 +10,14 @@ def index():
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/documentation')
+def documentation():
+    return render_template('documentation.html')
+
+@app.route('/query')
+def query():
+    return render_template('query.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
