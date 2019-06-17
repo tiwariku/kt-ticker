02105254b4708360765ddbc29909b360a21fdcf6
Kunal Tiwari <kunalltiwari@gmail.com>
2019-06-17 13:46:01 -0400

    backend to make dataframe and generate bokeh plot. some things have been hardcoded

--- app.py
+++ app.py
@@ -1,27 +1,75 @@
 from flask import Flask, render_template, request, redirect
 import requests
+from bokeh.plotting import figure
+from bokeh.embed import components
+import pandas as pd
 
 app = Flask(__name__)
 
 #decorator from Flask accepts route (where in the website you are) and directs to the correct html. Must function name match html file?
 @app.route('/')
 def index():
-    return render_template('query.html') #requires template file ot be in ./templates/
+    return render_template('index.html') #requires template file ot be in ./templates/
 
 @app.route('/query', methods=['GET', 'POST'])
 def query():
     if request.method == 'GET':
-        return render_template('query.html')
+        return render_template('index.html')
     elif request.method == 'POST':
-        return render_template('post.html',ticker=request.form['ticker'])#'you have requested: ' + str(request.form['ticker'])
+        ticker = request.form['ticker']
+        raw_json = get_ticker_data(ticker)
+        df = json_to_pandas_df(raw_json)
+        script, div = get_plot_script(df, ticker)
+        return render_template('graph.html', script=script, div=div)
+        return render_template('plot_display.html',
+                ticker=ticker,
+                api_url=df)
     else:
-        return str('UNKNOWN HTTP METHOD')
+        return str('UNKNOWN ROUTE')
 
 def get_ticker_data(ticker):
     '''
     internal function to handle lookup of ticker data
+    Quandl API usage:
+        GET https://www.quandl.com/api/v3/datasets/{database_code}/
+        {dataset_code}/data.{return_format}
+
     '''
-    return str(ticker)
+    #from TDI blog post, v1 API used, don't know about v3 advertised on quandl
+    url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % ticker
+    #this object handles retrieving data from apis. I thin there should be a way
+    #do this directly in pandas though
+    session = requests.Session()
+    #from TDI blog, this adapter says retry if you didn't make it to the
+    #route through https. it's just http in the TDI blog, unsure how defaults
+    # work in this regard
+    session.mount('https://', requests.adapters.HTTPAdapter(max_retries=3))
+    #retrieve response object from api, use .json() to convert to json-like
+    #dictionary
+    return session.get(url).json()
+
+def json_to_pandas_df(json):
+    '''
+    accepts json-like dict from quantl's api, and converts to a pandas
+    dataframe with the appropriate column names adn types
+    '''
+    #quandl json specific, there must be a better way to do this
+    df = pd.DataFrame(json['data'], columns=json['column_names'])
+    #convert to datetime, no timezone given
+    df['Date'] = pd.to_datetime(df['Date'])
+    return df
+
+def get_plot_script(df, ticker, plot_indeces=['Open']):
+    '''
+    accepts pandas dataframe with index Date and, by default, column 'Open'
+    '''
+    fig = figure(title='%s behaviour' % ticker,
+                x_axis_label='Time',
+                y_axis_label='Value')
+    fig.line(df['Date'],df[plot_indeces[0]])
+    script, div = components(fig)
+    return script, div
+    pass
 
 
 if __name__ == '__main__':
