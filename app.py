bb64969d1f0838ca9ee18f154ce1b8cf2a211878
Kunal Tiwari <kunalltiwari@gmail.com>
2019-06-17 14:25:15 -0400

    flush stdout

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ def json_to_pandas_df(json):
     dataframe with the appropriate column names adn types
     '''
     #quandl json specific, there must be a better way to do this
-    sys.stderr.write(str(json.keys()))
+    sys.stdout.write(str(json.keys()))
+    sys.stdout.flush()
     df = pd.DataFrame(json['data'], columns=json['column_names'])
     #convert to datetime, no timezone given
     df['Date'] = pd.to_datetime(df['Date'])
